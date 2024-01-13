2ff7611da3ea52e9d655d326ed2ac5479d528ee4
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2024-01-13 19:57:38 -0300

    Feat: nav bar

--- src/app/components/modal/navBar.js
+++ src/app/components/modal/navBar.js
@@ -1,14 +1,75 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { toggleModal } from '@/app/hook/toggleModal';
 
+const bgPerPageNoActive = ' dark:hover:bg-[#444CE6] '
+const bgPerPageActive = ' bg-[#444CE6] text-white'
+let svgPerPageNoActive = ' dark:text-gray-400 '
 
 
+// dark:text-white svg
 function NavBar ({ children, className = '' }) {
   const navBarRef = useRef(null);
 
   const toggleNavBar = toggleModal(navBarRef, 'main_nav_bar--open');
+
+  useEffect(() => {
+      const chageColorMenu = () => {
+
+         const aNodeList = document.querySelectorAll('.main_nav_bar a');
+         const pathname = window.location.pathname;
+
+         aNodeList.forEach((a) => {
+            const href = a.getAttribute('href');
+            const svgElement = a.querySelector('svg');
+            
+
+            if (href == pathname) {
+               a.classList.add('bg-[#444CE6]', 'text-white');
+
+               if (svgElement) {
+                  svgElement.classList.add('dark:text-white');
+               }
+            } else {
+               a.classList.add('dark:hover:bg-[#444CE6]');
+
+               if (svgElement) {
+                  svgElement.classList.add('dark:text-gray-400');
+               }
+            }
+         })
+      }
+
+      chageColorMenu()
+
+
+
+
+      const handleClickOutside = (event) => {
+         const navBar = navBarRef.current;
+   
+         let element = event.target;
+   
+         while (element) {
+            if (element.classList.contains('toggle_main_nav_bar')) {
+               return
+            }
+      
+            element = element.parentElement;
+         }
+   
+         if (navBar && !navBar.contains(event.target) && !event.target.classList.contains('main_nav_bar--open')) {
+         navBar.classList.remove('main_nav_bar--open');
+         }
+      };
+   
+      document.addEventListener('mousedown', handleClickOutside);
+   
+      return () => {
+         document.removeEventListener('mousedown', handleClickOutside);
+      }
+  }, []);
 
 
   return (
@@ -23,8 +84,16 @@ function NavBar ({ children, className = '' }) {
    <div class="h-full px-3 py-4 overflow-y-auto  ">
       <ul class="space-y-2 font-medium">
          <li>
-            <a href="#" class="flex dark:hover:dark:text-white text-white items-center p-2 rounded-lg  -hover:bg-gray-100 dark:bg-[#444CE6] group">
-               <svg class="w-5 h-5 text-white transition duration-75 dark:text-white group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+            <a 
+            href="/dashboard" 
+            class={
+               "flex dark:hover:dark:text-white  items-center p-2 rounded-lg  -hover:bg-gray-100 group "
+               }>
+               <svg 
+               class={
+                  "w-5 h-5 text-white transition duration-75  group-hover:text-gray-900 dark:group-hover:text-white"
+                  }
+                  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                   <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
                   <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
                </svg>
@@ -32,11 +101,11 @@ function NavBar ({ children, className = '' }) {
             </a>
          </li>
          <li>
-            <a href="#" class="flex dark:hover:dark:text-white items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 dark:hover:bg-[#444CE6] group">
+            <a href="/student" class="flex dark:hover:dark:text-white items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 dark:hover:bg-[#444CE6] group">
                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                   <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
                </svg>
-               <span class="flex-1 ms-3 whitespace-nowrap">Kanban</span>
+               <span class="flex-1 ms-3 whitespace-nowrap">Alunos</span>
                <span class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
             </a>
          </li>
