c7666fc179006e7d5d1f8b452be6b2e1efd162cb
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2024-08-08 22:46:22 -0300

    inserindo valores nas variaveis de registro de despesa/receita

--- src/app/components/modal/chatSuport.js
+++ src/app/components/modal/chatSuport.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import {useEffect, useRef, useState, use} from 'react';
 import './style.css';
 import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -18,6 +18,7 @@ import MiniConfig from './miniConfig';
 import { toggleModal } from '../../hook/toggleModal';
 import Select from 'react-select'
 import Switch from '@mui/material/Switch';
+import {useForm} from "react-hook-form";
 
 
 function ChatSuport({ menuRef }) {
@@ -26,12 +27,37 @@ function ChatSuport({ menuRef }) {
   const miniCofingAttachmentRef = useRef(null);
   const chatRef = useRef(null);
 
+  const [registerType, setRegisterType] = useState({
+    registro_efetivado: true
+  });
+
+  const { register, watch } = useForm()
+
+  // const allInput = watch(valor);
+
+  useEffect(() => {
+
+    console.log(registerType)
+
+    const subscription = watch((value, { name, type }) =>
+
+        setRegisterType({
+          ...registerType,
+          ...value
+        })
+    )
+    return () => subscription.unsubscribe()
+  }, [watch, registerType])
+
   const toggleMiniConfigLeft = toggleModal(miniCofingLeftRef, 'mini_config--open');
   const toggleMiniConfigRight = toggleModal(miniCofingRightRef, 'mini_config--open');
   const toggleMiniConfigAttachment = toggleModal(miniCofingAttachmentRef, 'mini_config--open');
 
-  const onSumitForm = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+
+    // console.log(registerType)
+    // console.log(e)
   }
 
   useEffect(() => {
@@ -79,20 +105,47 @@ function ChatSuport({ menuRef }) {
       {/*chat_suport_container*/}
 
       <div ref={chatRef}>
-        <form className="w-full px-4">
+        <form className="w-full px-4" onSubmit={handleSubmit}>
           <label htmlFor="valor" className="w-full">
             <input
-                id="valor" type="text" className="text-2xl font-medium" value="R$ 0,00" placeholder="R$ 0,00"/>
+                id="valor" type="text" {...register('value')} className="text-2xl font-medium" placeholder="R$ 0,00"/>
 
           </label>
 
           <div className="w-full gap-1 hover:border-[#141996] pb-1 mt-6 flex ">
             <button
-                className="darken-on-hover after:bg-[rgba(252_165_165/_0.4)] w-1/2 bg-[rgba(252_165_165/_0.3)] border-[rgba(252_165_165/_0.3)] border-b-2 border-b-red-700 rounded-none text-slate-950">
+                className="darken-on-hover after:bg-[rgba(252_165_165/_0.4)] w-1/2 bg-[rgba(252_165_165/_0.3)] border-[rgba(252_165_165/_0.3)] border-b-2 border-b-red-700 rounded-none text-slate-950"
+                onClick={() => {
+                  document.getElementById('tipo_registro').innerText = 'Foi pago';
+
+                  setRegisterType({
+                    ...registerType,
+                    tipo_registro: 'despesa'
+                  })
+                }}
+            >
               Despesa
             </button>
             <button
-                className="darken-on-hover after:bg-[rgba(134_239_172/_0.5)] w-1/2 bg-[rgba(156_163_175/_0.3)] border-[rgba(156_163_175/_0.3)] border-b-2 border-b-gray-700 rounded-none text-slate-950">
+                className="darken-on-hover after:bg-[rgba(134_239_172/_0.5)] w-1/2 bg-[rgba(156_163_175/_0.3)] border-[rgba(156_163_175/_0.3)] border-b-2 border-b-gray-700 rounded-none text-slate-950"
+                onClick={(e) => {
+                  // console.log(e.currentTarget.classList);
+
+                  e.currentTarget.classList.add(
+                      'darken-on-hover',
+                      'after:bg-[rgba(134_239_172/_0.4)]',
+                      'bg-[rgba(134_239_172/_0.3)]',
+                      'border-[rgba(134_239_172/_0.3)]',
+                      'border-b-green-700'
+                      );
+                  document.getElementById('tipo_registro').innerText = 'Foi recebido';
+
+                  setRegisterType({
+                    ...registerType,
+                    tipo_registro: 'receita'
+                  })
+                }}
+            >
               Receita
             </button>
           </div>
@@ -100,36 +153,96 @@ function ChatSuport({ menuRef }) {
           <section className="w-full gap-1 hover:border-[#141996] pb-1 mt-6 flex ">
 
             <div className="w-full gap-1 flex justify-between items-center px-6">
-              <p>Foi pago</p>
-              <Switch {...label} defaultChecked color="error" />
+              <p id="tipo_registro">Foi pago</p>
+              {
+                registerType?.tipo_registro === 'receita' ? (
+                    <Switch {...label} defaultChecked onChange={(e) => {
+                      setRegisterType({
+                        ...registerType,
+                        registro_efetivado: e.target.checked
+                      })
+                    }} color="success" />
+                ) : <Switch {...label} defaultChecked onChange={(e) => {
+                  setRegisterType({
+                    ...registerType,
+                    registro_efetivado: e.target.checked
+                  })
+                }} color="error" />
+              }
             </div>
 
           </section>
 
           <div className="w-full gap-1 border-b border-[#444CE6] hover:border-[#141996] pb-1 mt-6 flex ">
             <button
-                className={selectedDay}>
+                className={selectedDay}
+                onClick={(e) => {
+
+                  const date = new Date()
+                  date.setDate(date.getDate() - 1)
+
+                  setRegisterType({
+                    ...registerType,
+                    quando_ocorreu: date.toISOString()
+                  })
+                }}
+            >
               Hoje
             </button>
             <button
-                className={noSelectedDay}>
+                className={noSelectedDay}
+                onClick={(e) => {
+
+                  const date = new Date()
+                  date.setDate(date.getDate() - 2)
+
+                  setRegisterType({
+                    ...registerType,
+                    quando_ocorreu: date.toISOString()
+                  })
+                }}>
               Ontem
             </button>
 
             <button
-                className={noSelectedDay}>
+                className={noSelectedDay}
+                onClick={(e) => {
+
+                  // precisa chamar uma funcao assincrona puxando um modal com full calendar, para que o user escolha a data que ele quiser
+
+                  setRegisterType({
+                    ...registerType,
+                    quando_ocorreu: '2024-04-12'
+                  })
+                }}>
               Outro dia
             </button>
           </div>
 
           <label htmlFor="description" className="w-full mt-6">
-            <input id="description" type="text" className="font-medium" placeholder="Descrição"/>
+            <input id="description" {...register('description')} type="text" className="font-medium" placeholder="Descrição"/>
 
           </label>
 
-          <Select options={optionsBank} placeholder="Escolha um banco" className="w-full text-left mt-6"/>
+          <Select
+              options={optionsBank}
+
+              onChange={(e) => {
+                setRegisterType({
+                  ...registerType,
+                  banco: e.value
+                })
+              }}
+
+              placeholder="Escolha um banco" className="w-full text-left mt-6"/>
 
           <Select options={optionsTag} placeholder="Escolha uma Tag" styles="border: none; "
+              onChange={(e) => {
+                setRegisterType({
+                  ...registerType,
+                  tag: e.value
+                })
+              }}
                   className="w-full text-left mt-6 rounded-none "/>
 
           <div className="w-full gap-1 pb-6 mt-6 flex justify-center ">
